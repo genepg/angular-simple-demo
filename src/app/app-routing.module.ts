cc5c52668ff41323657bc42758da4a76338ef252
PGboy <zscfde@gmail.com>
2019-03-01 12:54:28 +0800

    chore: add fileUpload path & set PreloadAllModules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'fileUpload',
+    loadChildren: './file-upload/file-upload.module#FileUploadModule'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    // component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
