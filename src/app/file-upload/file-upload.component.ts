4de274b813c2fb29c58fe834e29b6fb1b269bdde
PGboy <zscfde@gmail.com>
2019-06-28 15:40:04 +0800

    chore: update angular material

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog } from '@angular/material/dialog';
 
 import { UploadDialogComponent } from './upload-dialog/upload-dialog.component';
 
--- src/app/file-upload/upload-dialog/upload-dialog.component.ts
+++ src/app/file-upload/upload-dialog/upload-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, Inject } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { forkJoin, Observable } from 'rxjs';
 import { UploadService } from './upload.service';
 
